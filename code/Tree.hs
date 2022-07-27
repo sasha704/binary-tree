d0fc46ddf9e151b2c8bed7abb0120a61f730ba2d
sasha704 <alg26@st-andrews.ac.uk>
2022-07-27 15:43:48 +0100

    search method added, remove and add started

---
+++ code/Tree.hs
@@ -0,0 +1,45 @@
+-- Tree data is either a branch to two new trees, and a value, or is NONE
+data Tree = Branch Tree Tree Int
+          | None
+          deriving (Eq, Show)
+
+-- add a value to the tree
+addBranch :: Int -> Tree -> Tree
+-- if there are no values in the tree already, just add the value as the root node
+addBranch newValue (None) = Branch None None newValue
+-- if there are values in the tree already, add a new branch accordingly
+--addBranch newValue (Branch left right branchValue) =  
+
+
+-- Check if an integer is already in a tree
+findValue :: Int -> Tree -> Bool
+findValue searchValue (None) = False
+findValue searchValue (Branch left right branchValue) =
+    if searchValue == branchValue -- if the current node is the right one
+        then True
+    else
+        if searchValue > branchValue -- if the search value is bigger than the node
+            then findValue searchValue right -- search right
+        else findValue searchValue left -- else search left
+
+
+removeBranch :: Int -> Tree -> Tree
+removeBranch value (Branch left right branchValue) =
+        if value == branchValue -- if the current node is the right one
+            then 
+                if left == None -- (the left side has no values)
+                    then
+                        if right == None
+                            --(no children)
+                            then None
+                        else right -- (only the right side has values)
+                else -- (the left side has values)
+                    if right == None
+                        then left -- (only left has values)
+                    else (Branch left right  -- (both sides have values)
+                    --- UNFINISHED
+                        
+        else
+            if value > branchValue -- if the value is bigger than the node
+                then Branch left (removeBranch value right) -- search right
+            else Branch (removeBranch value left) right -- else search left
